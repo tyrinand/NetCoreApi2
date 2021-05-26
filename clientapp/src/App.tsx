e20be88f134c52b1f23854d7702c9ae4ed6a02ab
tyrinand <tyrinand@yandex.ru>
2021-05-26 23:54:33 +0300

    add lazy loader

--- clientapp/src/App.tsx
+++ clientapp/src/App.tsx
@@ -1,19 +1,23 @@
-import Info from './../src/Components/Info';
 import {BrowserRouter, Switch, Route, NavLink} from 'react-router-dom';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
-import Softs from './Components/Softs/Softs';
-import Sales from './Components/Sales/Sales';
-import Clients from './Components/Clients/Clients';
 import './App.css';
-import FormClient from '../src/Components/Clients/FormClient';
 import { reactUrlClients, reactUrlLogs, reactUrlSales, reactUrlSofts } from './Interface/types';
-import FormSoft from './Components/Softs/FormSoft';
-import FormSale from './Components/Sales/FormSale';
-import Logs from './Components/Logs/Logs';
+import { lazy, Suspense } from 'react';
+import CircularProgress from '@material-ui/core/CircularProgress';
+
+const Logs = lazy(() => import('./Components/Logs/Logs'));
+const FormSale = lazy(() => import('./Components/Sales/FormSale'));
+const FormSoft = lazy(() => import('./Components/Softs/FormSoft'));
+const FormClient = lazy(() => import('./../src/Components/Clients/FormClient'));
+const Clients = lazy(() => import('./Components/Clients/Clients'));
+const Sales = lazy(() => import('./Components/Sales/Sales'));
+const Softs = lazy(() => import('./Components/Softs/Softs'));
+const Info = lazy(() => import('./../src/Components/Info'));
+
 
 
 const useStyles = makeStyles( (theme) =>  ({
@@ -37,7 +41,10 @@ const useStyles = makeStyles( (theme) =>  ({
   },
 }));
 
-
+const loader = (
+  <Grid container spacing={0} justify="center">
+    <CircularProgress disableShrink />
+  </Grid>);
 
 function App() {
   const classes = useStyles();
@@ -62,30 +69,32 @@ function App() {
   <br/>
   <Grid container spacing={0} justify="center">
     <Grid item lg={8}  md={12}  xs={12} sm={12}>
-    <Switch>
-        <Route path="/"       component={Info}  exact/>
-        
-        <Route path = {`/${reactUrlClients}`}            component = {Clients} exact />
-        <Route path = {`/${reactUrlClients}/page/:page`} component = {Clients} />
-        <Route path = {`/${reactUrlClients}/create`}     component = {FormClient} />
-        <Route path = {`/${reactUrlClients}/edit/:id`}   component = {FormClient} />
+    <Suspense fallback={loader} >
+      <Switch>
+          <Route path="/"       component={Info}  exact/>
+          
+          <Route path = {`/${reactUrlClients}`}            component = {Clients} exact />
+          <Route path = {`/${reactUrlClients}/page/:page`} component = {Clients} />
+          <Route path = {`/${reactUrlClients}/create`}     component = {FormClient} />
+          <Route path = {`/${reactUrlClients}/edit/:id`}   component = {FormClient} />
 
-        <Route path = {`/${reactUrlSofts}`}             component = {Softs} exact />
-        <Route path = {`/${reactUrlSofts}/page/:page`}  component = {Softs} />
-        <Route path = {`/${reactUrlSofts}/create`}      component = {FormSoft} />
-        <Route path = {`/${reactUrlSofts}/edit/:id`}    component = {FormSoft} />
+          <Route path = {`/${reactUrlSofts}`}             component = {Softs} exact />
+          <Route path = {`/${reactUrlSofts}/page/:page`}  component = {Softs} />
+          <Route path = {`/${reactUrlSofts}/create`}      component = {FormSoft} />
+          <Route path = {`/${reactUrlSofts}/edit/:id`}    component = {FormSoft} />
 
-        <Route path = {`/${reactUrlSales}`}            component = {Sales} exact />
-        <Route path = {`/${reactUrlSales}/page/:page`} component = {Sales} />
-        <Route path = {`/${reactUrlSales}/create`}     component = {FormSale} />
-        <Route path = {`/${reactUrlSales}/edit/:id`}   component = {FormSale} />
+          <Route path = {`/${reactUrlSales}`}            component = {Sales} exact />
+          <Route path = {`/${reactUrlSales}/page/:page`} component = {Sales} />
+          <Route path = {`/${reactUrlSales}/create`}     component = {FormSale} />
+          <Route path = {`/${reactUrlSales}/edit/:id`}   component = {FormSale} />
 
-        <Route path = {`/${reactUrlLogs}`}   component = {Logs} exact />
-        <Route path = {`/${reactUrlLogs}/page/:page`} component = {Logs} exact/>
-        <Route path = {`/${reactUrlLogs}/page/:page/filter/:filter`} component = {Logs} />
+          <Route path = {`/${reactUrlLogs}`}   component = {Logs} exact />
+          <Route path = {`/${reactUrlLogs}/page/:page`} component = {Logs} exact/>
+          <Route path = {`/${reactUrlLogs}/page/:page/filter/:filter`} component = {Logs} />
 
-        <Route  component={Info} />
-      </Switch>
+          <Route  component={Info} />
+        </Switch>
+      </Suspense>
     </Grid>
   </Grid>
       <footer className={classes.footer}>
